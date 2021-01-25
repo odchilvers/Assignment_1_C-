39e20c063ae7b0060121de5ab856a5e7075b99d9
Olivia Chilvers <69412454+odchilvers@users.noreply.github.com>
2021-01-25 14:08:25 -0800

    Update ProblemFireCapacity.cpp

--- ProblemFireCapacity.cpp
+++ ProblemFireCapacity.cpp
@@ -1,4 +1,6 @@
 // Fire Capacity
+// this program determines whether the number of people attending a meeting meets room capcity or exceeds it due to a fire hazard
+// it calculates how many more people may attend or how many will need to leave
 
 #include <iostream>
 
@@ -8,16 +10,20 @@ int main() {
   int maxRoomCapacity;
   int numOfPeople;
   int numUntilMax;
-
+// asks the user for the maximum room capacity
   cout << "Enter the maximum room capacity: " << endl;
+  // stores that vallue into maxRoomCapacity
   cin >> maxRoomCapacity;
+  // asks the number of people attending
   cout << "Enter the number of people attending: " << endl;
+  // stores that value into numOfPeople
   cin >> numOfPeople;
 // checks if number of people fits room capacity
 if (numOfPeople <= maxRoomCapacity) {
   // calcutes number of people that can attend until max occupancy
   numUntilMax = maxRoomCapacity - numOfPeople;
   cout << "Legal: Meeting may proceed." << endl;
+  // prints out how many more people may attend
   cout << numUntilMax << " more people may attend." << endl;
 }
 // runs if too many people want to enter the room
@@ -25,6 +31,7 @@ else {
   // calculates how many people must be excluded to return to max capacity
   numUntilMax = numOfPeople - maxRoomCapacity;
   cout << "Illegal: Fire Hazard! Too many people in room." << endl;
+  // calculates how many people will need to leave
   cout << numUntilMax << " people must be excluded in order to meet the fire regulations." << endl;
 }
   return 0;
